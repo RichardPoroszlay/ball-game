6a21ef1495f1ff0480e086a8e2e7298141e5f1fb
Richard Poroszlay <richardporoszlay@gmail.com>
2023-04-23 16:10:10 +0200

    Create a constructor in the Board class, which initializes the game board for us with the walls, the finish field and the ball placed on it.

--- ball-game-model/src/main/java/Board.java
+++ ball-game-model/src/main/java/Board.java
@@ -1,4 +1,49 @@
 public class Board {
     private Field[][] fields;
     private Ball ball;
+
+    public Board() {
+        fields = new Field[7][7];
+
+        fields[0][0].setWallRight(true);
+        fields[0][1].setWallLeft(true);
+        fields[0][2].setWallBottom(true);
+        fields[0][3].setWallRight(true);
+        fields[0][4].setWallLeft(true);
+        fields[0][6].setWallBottom(true);
+        fields[1][2].setWallTop(true);
+        fields[1][6].setWallTop(true);
+        fields[2][1].setWallBottom(true);
+        fields[2][2].setWallRight(true);
+        fields[2][3].setWallLeft(true);
+        fields[2][5].setWallRight(true);
+        fields[2][6].setWallLeft(true);
+        fields[3][1].setWallTop(true);
+        fields[3][3].setWallRight(true);
+        fields[3][3].setWallBottom(true);
+        fields[3][4].setWallLeft(true);
+        fields[3][4].setWallRight(true);
+        fields[3][5].setWallLeft(true);
+        fields[3][6].setWallBottom(true);
+        fields[4][0].setWallBottom(true);
+        fields[4][3].setWallTop(true);
+        fields[4][4].setWallBottom(true);
+        fields[4][6].setWallTop(true);
+        fields[5][0].setWallTop(true);
+        fields[5][1].setWallRight(true);
+        fields[5][2].setWallLeft(true);
+        fields[5][2].setWallBottom(true);
+        fields[5][2].setWallRight(true);
+        fields[5][3].setWallLeft(true);
+        fields[5][4].setWallTop(true);
+        fields[6][2].setWallTop(true);
+        fields[6][3].setWallRight(true);
+        fields[6][4].setWallLeft(true);
+        fields[6][5].setWallRight(true);
+        fields[6][6].setWallLeft(true);
+
+        fields[5][2].setFinish(true);
+
+        ball = new Ball(1,4);
+    }
 }
