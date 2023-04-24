4197b4e7ec1638d6c0f895eb922e42f812be7796
Richard Poroszlay <richardporoszlay@gmail.com>
2023-04-24 19:53:37 +0200

    Add documentation to the Ball class

--- ball-game-model/src/main/java/Ball.java
+++ ball-game-model/src/main/java/Ball.java
@@ -1,6 +1,10 @@
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Class representing the ball and its position in the game.
+ * @author Richard Poroszlay
+ */
 @Data
 @AllArgsConstructor
 public class Ball {
