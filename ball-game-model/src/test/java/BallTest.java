22cd3e49d6516da2a8847e86888f2f5bd2c504fa
Richard Poroszlay <richardporoszlay@gmail.com>
2023-04-25 16:33:01 +0200

    Add a lifecycle method to BallTest.java

--- ball-game-model/src/test/java/BallTest.java
+++ ball-game-model/src/test/java/BallTest.java
@@ -1,8 +1,12 @@
 import ballgame.model.Ball;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BallTest {
     private Ball ball;
 
-
+    @BeforeEach
+    public void setUp() {
+        Ball ball = new Ball(3,4);
+    }
 }
