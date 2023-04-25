fe398a5cd0b66941ae8467a4c3e58d7d04bd0202
Richard Poroszlay <richardporoszlay@gmail.com>
2023-04-25 23:01:20 +0200

    Remove TODO from FieldTest.java

--- ball-game-model/src/test/java/FieldTest.java
+++ ball-game-model/src/test/java/FieldTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FieldTest {
-    //TODO
     @Test
     public void finishFieldShouldBeTrue() {
         var field = new Field();
