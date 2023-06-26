d44dc5d72127bcd4bb84e57982e432c6bfa5c465
Richard Poroszlay <richardporoszlay@gmail.com>
2023-06-26 16:49:22 +0200

    Set up Ball.java

--- src/main/java/ballgame/model/Ball.java
+++ src/main/java/ballgame/model/Ball.java
@@ -1,4 +1,31 @@
 package ballgame.model;
 
+
 public class Ball {
+    private int row;
+    private int col;
+
+    public Ball() {
+    }
+
+    public Ball(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
 }
