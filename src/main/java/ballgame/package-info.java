0cfc1f068633ca8b360907446875b2c4274838ca
Richard Poroszlay <richardporoszlay@gmail.com>
2023-06-26 16:46:00 +0200

    Add package-info.java to the ballgame package

---
+++ src/main/java/ballgame/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the view and the controller layer of the game.
+ * Moreover, the Main executable file is also located in this package.
+ *
+ * @author Richard Poroszlay
+ */
+package ballgame;
