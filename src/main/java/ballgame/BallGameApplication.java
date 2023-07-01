89ef6fc05a4499821c6ac2a9ef56cd2df3fc4430
Richard Poroszlay <richardporoszlay@gmail.com>
2023-07-01 19:27:37 +0200

    Set up BallGameApplication.java

--- src/main/java/ballgame/BallGameApplication.java
+++ src/main/java/ballgame/BallGameApplication.java
@@ -1,4 +1,25 @@
 package ballgame;
 
-public class BallGameApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class BallGameApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/ui.fxml"));
+        stage.setTitle("Ball Game");
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/ui.css").toExternalForm());
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
 }
