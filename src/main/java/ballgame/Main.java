e2600f9cb3d9e04a29a18d971f3270934d142ae4
Richard Poroszlay <richardporoszlay@gmail.com>
2023-07-01 19:31:58 +0200

    Set up Main.java

--- src/main/java/ballgame/Main.java
+++ src/main/java/ballgame/Main.java
@@ -1,4 +1,11 @@
 package ballgame;
 
+import javafx.application.Application;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(BallGameApplication.class, args);
+    }
+
 }
