61ef36f35208fb3c7fff9e5b7a2d9054f1a261f0
Richard Poroszlay <richardporoszlay@gmail.com>
2023-07-03 19:49:32 +0200

    Set up GameResult with documentation

--- src/main/java/ballgame/result/GameResult.java
+++ src/main/java/ballgame/result/GameResult.java
@@ -1,4 +1,23 @@
 package ballgame.result;
 
+import lombok.*;
+
+import java.time.ZonedDateTime;
+
+/**
+ * This class describes the properties of a player, who played the game.
+ *
+ * @author Richard Poroszlay
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GameResult {
+
+    @NonNull private String playerName;
+    private boolean solved;
+    private int steps;
+    @NonNull private ZonedDateTime created;
+
 }
