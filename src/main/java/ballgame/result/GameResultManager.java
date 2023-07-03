fd59f6ab3b20c399f0728969bea31cf11081a6a8
Richard Poroszlay <richardporoszlay@gmail.com>
2023-07-03 19:56:31 +0200

    Set up GameResultManager with documentation

--- src/main/java/ballgame/result/GameResultManager.java
+++ src/main/java/ballgame/result/GameResultManager.java
@@ -1,4 +1,29 @@
 package ballgame.result;
 
-public class GameResultManager {
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+
+
+/**
+ * This interface has methods that will be implemented in JsonGameResultManager.java.
+ * These are essential methods for json data management.
+ *
+ * @author Richard Poroszlay
+ */
+public interface GameResultManager {
+
+    List<GameResult> add(GameResult result) throws IOException;
+
+    List<GameResult> getAll() throws IOException;
+
+    default List<GameResult> getBest(int limit) throws IOException {
+        return getAll()
+                .stream()
+                .filter(GameResult::isSolved)
+                .sorted(Comparator.comparingInt(GameResult::getSteps))
+                .limit(limit)
+                .toList();
+    }
+
 }
