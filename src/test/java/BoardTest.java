3c35848a3fcb2bcfec3a4b9e90d8a465cd9d12b8
Richard Poroszlay <richardporoszlay@gmail.com>
2023-07-06 20:05:49 +0200

    Set up BoardTest

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,2 +1,26 @@
+import ballgame.model.Board;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BoardTest {
+
+    private Board board;
+
+    @BeforeEach
+    public void setUp() {
+        board = new Board();
+    }
+
+    @Test
+    public void isStepIncreasedByOne() {
+        board.addOneToStepCounter();
+        assertEquals(1, board.getStepCounter());
+    }
+
+    @Test
+    public void testGetStepCounter() {
+        assertEquals(0, board.getStepCounter());
+    }
 }
