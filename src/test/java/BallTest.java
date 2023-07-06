935b07f6f76e07a8f93661d18fbf7cf2cf573e03
Richard Poroszlay <richardporoszlay@gmail.com>
2023-07-06 20:05:00 +0200

    Set up BallTest

--- src/test/java/BallTest.java
+++ src/test/java/BallTest.java
@@ -1,2 +1,37 @@
+import ballgame.model.Ball;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BallTest {
+
+    private Ball ball;
+
+    @BeforeEach
+    public void setUp() {
+        ball = new Ball(1,4);
+    }
+
+    @Test
+    public void testGetRow() {
+        assertEquals(1, ball.getRow());
+    }
+
+    @Test
+    public void testGetCol() {
+        assertEquals(4, ball.getCol());
+    }
+
+    @Test
+    public void testSetRow() {
+        ball.setRow(2);
+        assertEquals(2, ball.getRow());
+    }
+
+    @Test
+    public void testSetCol() {
+        ball.setCol(3);
+        assertEquals(3, ball.getCol());
+    }
 }
